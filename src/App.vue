7a337c8084ba963682a3d9ff942c4b7ab9b4124d
aki <247557768@qq.com>
2021-11-04 17:04:18 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>123</div>
-  <div></div>
+  <div>G6</div>
+  <G6Test1></G6Test1>
 </template>
 
 <script>
+  import G6Test1 from "./components/G6Test1.vue";
+
 export default {
   name: 'App',
   components: {
+    G6Test1
   }
 }
 </script>
